616f615bb864139709fd255937920d99dff3600b
aoki ken <aokiken@me.com>
2024-10-07 21:48:49 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# vue-v-model-input-type-number-testing
+vue2とvue3でv-modelを使ったinput type="number"のテスト
---
+++ vue2-env/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <h2>Vue 2({{ version }})</h2>
+    <input type="number" v-model="value">
+    <p>値は {{ typeof value }} 型です。</p>
+    <p>value: {{ value }}</p>
+  </div>
+</template>
+<script>
+import { version } from 'vue'
+export default {
+  name: 'App',
+  data(){
+    return {
+      value: '',
+      version,
+    }
+  },
+}
+</script>
+
+<style>
+</style>
